4d9c85305e6254ae6c4c27290b233de0e01505ca
sk2kt <130651719+sk2kt@users.noreply.github.com>
2023-06-04 17:27:18 +0300

    Update create.js

--- ScriptFolder/create.js
+++ ScriptFolder/create.js
@@ -20,50 +20,51 @@ context.fillRect(0, canvas.height / 2, canvas.width, canvas.height / 2);
 
 
 items.forEach(function (item) {
-    item.addEventListener("mousedown", function (e) {
-        draggedItem = item.cloneNode(true);
-        offsetX = e.offsetX;
-        offsetY = e.offsetY;
-        document.body.appendChild(draggedItem);
-    });
-});
-
-document.addEventListener("mousemove", function (e) {
-    if (draggedItem) {
-        draggedItem.style.position = "absolute";
-        draggedItem.style.left = `${e.pageX - offsetX}px`;
-        draggedItem.style.top = `${e.pageY - offsetY}px`;
-    }
-});
-
-document.addEventListener("mouseup", function (e) {
-    if (draggedItem) {
-        if (e.clientX > canvasRect.left && e.clientX < canvasRect.right && e.clientY > canvasRect.top && e.clientY < canvasRect.bottom) {
-            draggedItem.style.left = `${e.pageX - canvasRect.left - offsetX}px`;
-            draggedItem.style.top = `${e.pageY - canvasRect.top - offsetY}px`;
-            canvas.appendChild(draggedItem);
-        } else {
-            document.body.removeChild(draggedItem);
-        }
-        draggedItem = null;
-    }
-});
-
-document.querySelector('#save-btn').addEventListener('click', function() {
-  html2canvas(document.querySelector("#canvas")).then(canvas => {
-    let link = document.createElement('a');
-    link.download = 'myworld.png';
-    link.href = canvas.toDataURL();
-    link.click();
+  item.addEventListener("mousedown", function (e) {
+      draggedItem = item.cloneNode(true);
+      offsetX = e.offsetX;
+      offsetY = e.offsetY;
+      document.body.appendChild(draggedItem);
   });
 });
 
+document.addEventListener("mousemove", function (e) {
+  if (draggedItem) {
+      draggedItem.style.position = "absolute";
+      draggedItem.style.left = `${e.pageX - offsetX}px`;
+      draggedItem.style.top = `${e.pageY - offsetY}px`;
+  }
+});
+
+document.addEventListener("mouseup", function (e) {
+  if (draggedItem) {
+      if (e.clientX > canvasRect.left && e.clientX < canvasRect.right && e.clientY > canvasRect.top && e.clientY < canvasRect.bottom) {
+          draggedItem.style.left = `${e.pageX - canvasRect.left - offsetX}px`;
+          draggedItem.style.top = `${e.pageY - canvasRect.top - offsetY}px`;
+          canvas.appendChild(draggedItem);
+      } else {
+          document.body.removeChild(draggedItem);
+      }
+      draggedItem = null;
+  }
+});
+
+document.querySelector('#save-btn').addEventListener('click', function() {
+html2canvas(document.querySelector("#canvas")).then(canvas => {
+  let link = document.createElement('a');
+  link.download = 'myworld.png';
+  link.href = canvas.toDataURL();
+  link.click();
+});
+});
+
 document.querySelector('#home-btn').addEventListener('click', function() {
-  window.location.href = 'index.html'; 
+window.location.href = 'index.html'; 
 });
 
 document.querySelector('#undo-btn').addEventListener('click', function() {
-  if (canvas.lastChild) {
-    canvas.removeChild(canvas.lastChild);
-  }
+if (canvas.lastChild) {
+  canvas.removeChild(canvas.lastChild);
+}
 });
+
