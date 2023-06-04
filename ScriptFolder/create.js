ecac11e90503bd42e2b966296fdca23316270ece
sk2kt <130651719+sk2kt@users.noreply.github.com>
2023-06-04 16:09:20 +0300

    upd

--- ScriptFolder/create.js
+++ ScriptFolder/create.js
@@ -4,25 +4,22 @@ let canvasRect = canvas.getBoundingClientRect();
 let offsetX, offsetY;
 let draggedItem = null;
 
+function mouseDownHandler(e) {
+    draggedItem = this.cloneNode(true);
+    offsetX = e.offsetX;
+    offsetY = e.offsetY;
+    document.body.appendChild(draggedItem);
+}
 
-items.forEach(function (item) {
-    item.addEventListener("mousedown", function (e) {
-        draggedItem = item.cloneNode(true);
-        offsetX = e.offsetX;
-        offsetY = e.offsetY;
-        document.body.appendChild(draggedItem);
-    });
-});
-
-document.addEventListener("mousemove", function (e) {
+function mouseMoveHandler(e) {
     if (draggedItem) {
         draggedItem.style.position = "absolute";
         draggedItem.style.left = `${e.pageX - offsetX}px`;
         draggedItem.style.top = `${e.pageY - offsetY}px`;
     }
-});
+}
 
-document.addEventListener("mouseup", function (e) {
+function mouseUpHandler(e) {
     if (draggedItem) {
         if (e.clientX > canvasRect.left && e.clientX < canvasRect.right && e.clientY > canvasRect.top && e.clientY < canvasRect.bottom) {
             draggedItem.style.left = `${e.pageX - canvasRect.left - offsetX}px`;
@@ -33,23 +30,33 @@ document.addEventListener("mouseup", function (e) {
         }
         draggedItem = null;
     }
+}
+
+items.forEach(function (item) {
+    item.addEventListener("mousedown", mouseDownHandler);
 });
 
+document.addEventListener("mousemove", mouseMoveHandler);
+
+document.addEventListener("mouseup", mouseUpHandler);
+
+document.addEventListener("mouseout", mouseUpHandler);
+
 document.querySelector('#save-btn').addEventListener('click', function() {
-  html2canvas(document.querySelector("#canvas")).then(canvas => {
-    let link = document.createElement('a');
-    link.download = 'myworld.png';
-    link.href = canvas.toDataURL();
-    link.click();
-  });
+    html2canvas(document.querySelector("#canvas")).then(canvas => {
+        let link = document.createElement('a');
+        link.download = 'myworld.png';
+        link.href = canvas.toDataURL("image/png");
+        link.click();
+    });
 });
 
 document.querySelector('#home-btn').addEventListener('click', function() {
-  window.location.href = 'index.html'; 
+    window.location.href = 'index.html'; 
 });
 
 document.querySelector('#undo-btn').addEventListener('click', function() {
-  if (canvas.lastChild) {
-    canvas.removeChild(canvas.lastChild);
-  }
+    if (canvas.lastChild) {
+        canvas.removeChild(canvas.lastChild);
+    }
 });
