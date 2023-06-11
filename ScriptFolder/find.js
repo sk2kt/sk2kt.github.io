7ec32312cc7b98c400324c14626897d6ee4c0c5d
sk2kt <130651719+sk2kt@users.noreply.github.com>
2023-06-11 13:33:24 +0300

    Sm upd

--- ScriptFolder/find.js
+++ ScriptFolder/find.js
@@ -1,8 +1,8 @@
 window.onload = function() {
     const canvas = document.getElementById("gameCanvas");
     const ctx = canvas.getContext("2d");
-    canvas.width = 600;
-    canvas.height = 600;
+    canvas.width = 700;
+    canvas.height = 400;
   
     const objectsToFindDiv = document.getElementById("objectsToFind");
     
